512744dab695e5fd35c4a1942360c2bf45b04144
László Szűcs <laszlo.szucs@klarna.com>
2019-02-23 08:57:10 +0100

    feat(index.ts): Generate typings for components.schemas
    Fill typeStore with compiled schemas

--- lib/index.spec.ts
+++ lib/index.spec.ts
@@ -10,7 +10,18 @@ describe('openapi-typescript', () => {
         paths: {}
       }
       it('should respond nothing', async () => {
-        expect((await GenerateTypes(schema))).toMatch(/\s*/)
+        const typings = await GenerateTypes(schema)
+        expect(typings).toMatch(/^\s*$/)
+      })
+    })
+
+    describe('given the Petstore schema', () => {
+      const schema = require('../fixtures/petstore.json')
+
+      it('should output typesfor components.schemas ', async () => {
+        const typings = await GenerateTypes(schema)
+        expect(typings).toContain('export interface Pet')
+        expect(typings).toContain('export type Pets = (Pet)[]')
       })
     })
   })
--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,25 @@
 import 'source-map-support/register'
 import { InternalRefRewriter } from './refs'
+import { compileSchema } from './compile'
 
 type StringStore = { [key:string]:string }
 
 export const GenerateTypes = async (parsedOpenAPISchema:OpenAPISchema):Promise<string> => {
+  const { components: { schemas }} = parsedOpenAPISchema
   const typeStore:StringStore = {}
 
   new InternalRefRewriter().rewrite(parsedOpenAPISchema.components.schemas)
   new InternalRefRewriter().rewrite(parsedOpenAPISchema.paths)
-  return `
 
-  `
+  for (const schemaName of Object.keys(schemas)) {
+    typeStore[schemaName] = await compileSchema(schemas[schemaName], schemaName)
+  }
+
+  return removeMagic(Object.values(typeStore).join('\n'))
+}
+
+function removeMagic(line: string):string {
+  return line.replace(
+    /"\$magic\$[^"]+"/g, 
+    found => found.substr(8, found.length - 9))
 }
