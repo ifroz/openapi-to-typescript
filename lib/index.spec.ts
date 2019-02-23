e6396b512ff24660c1ead34d6c025f0c8f278761
László Szűcs <laszlo.szucs@klarna.com>
2019-02-23 19:52:18 +0100

    test(index.spec.ts): Remove top level describe() wrap

--- lib/index.spec.ts
+++ lib/index.spec.ts
@@ -1,41 +1,38 @@
 import { GenerateTypings } from './index'
 
-describe('openapi-typescript', () => {
-  describe('GenerateTypings', () => {
-    describe('given an empty openapi schema', () => {
-      const schema = {
-        components: {
-          schemas: {}
-        },
-        paths: {}
-      }
-      itShouldGenerateValidTypingsFromSchema(schema)
-      it('should respond nothing', async () => {
-        const typings = await GenerateTypings(schema)
-        expect(typings).toMatch(/^\s*$/)
-        expect(typings).toEqual('')
-      })
+describe('GenerateTypings', () => {
+  describe('given an empty openapi schema', () => {
+    const schema = {
+      components: {
+        schemas: {}
+      },
+      paths: {}
+    }
+    itShouldGenerateValidTypingsFromSchema(schema)
+    it('should respond nothing', async () => {
+      const typings = await GenerateTypings(schema)
+      expect(typings).toMatch(/^\s*$/)
+      expect(typings).toEqual('')
     })
+  })
 
-    describe('given the Petstore schema', () => {
-      const schema = require('../fixtures/petstore.json')
-      itShouldGenerateValidTypingsFromSchema(schema)
-      it('should output types for components.schemas ', async () => {
-        const typings = await GenerateTypings(schema)
-        expect(typings).toContain('export interface Pet')
-        expect(typings).toContain('export type Pets = (Pet)[]')
-      })
+  describe('given the Petstore schema', () => {
+    const schema = require('../fixtures/petstore.json')
+    itShouldGenerateValidTypingsFromSchema(schema)
+    it('should output types for components.schemas ', async () => {
+      const typings = await GenerateTypings(schema)
+      expect(typings).toContain('export interface Pet')
+      expect(typings).toContain('export type Pets = (Pet)[]')
     })
+  })
 
-    describe('given the Petstore Expanded schema', () => {
-      const schema = require('../fixtures/petstoreExpanded.json')
-      itShouldGenerateValidTypingsFromSchema(schema)
-      it('should output types for components.schemas ', async () => {
-        const typings = await GenerateTypings(schema)
-        expect(typings).toContain('export type Pet = NewPet & {')
-      })
+  describe('given the Petstore Expanded schema', () => {
+    const schema = require('../fixtures/petstoreExpanded.json')
+    itShouldGenerateValidTypingsFromSchema(schema)
+    it('should output types for components.schemas ', async () => {
+      const typings = await GenerateTypings(schema)
+      expect(typings).toContain('export type Pet = NewPet & {')
     })
-
   })
 })
 
