65205cc67932f9a8416bf79e766a8092b2ddd5b2
László Szűcs <laszlo.szucs@klarna.com>
2019-02-24 19:24:12 +0100

    refactor(store.ts): Replace typeStore object with a class

--- lib/index.ts
+++ lib/index.ts
@@ -1,29 +1,29 @@
 import 'source-map-support/register'
+
+import { Store } from './store'
 import { InternalRefRewriter } from './refs'
 import { Operation } from './operation'
 import { defaultOperationFormatters, SchemaFormatter } from './formatters'
 
-type StringStore = { [key:string]:string }
-
 export const GenerateTypings = async (parsedOpenAPISchema:OpenAPISchema):Promise<string> => {
   const { paths, components: { schemas }} = parsedOpenAPISchema
-  const typeStore:StringStore = {}
+  const typeStore = new Store<string>()
 
   new InternalRefRewriter().rewrite(parsedOpenAPISchema.components.schemas)
   new InternalRefRewriter().rewrite(parsedOpenAPISchema.paths)
 
   for (const schemaName of Object.keys(schemas)) {
-    Object.assign(typeStore, await new SchemaFormatter(schemas[schemaName], schemaName).render())
+    typeStore.assign(await new SchemaFormatter(schemas[schemaName], schemaName).render())
   }
 
   for (const pathName of Object.keys(paths)) {
     for (const method of Object.keys(paths[pathName])) {
       const operation = new Operation(paths[pathName][method], { pathName, method })
       for (const Formatter of defaultOperationFormatters) {
-        Object.assign(typeStore, await new Formatter(operation).render())
+        typeStore.assign(await new Formatter(operation).render())
       }
     }
   }
 
-  return Object.values(typeStore).filter(t => t).join('\n')
+  return typeStore.toString()
 }
