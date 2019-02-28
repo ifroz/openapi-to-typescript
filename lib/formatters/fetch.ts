444e4a4a51f4254732e1dabb6515b2df256835f2
László Szűcs <laszlo.szucs@klarna.com>
2019-02-28 22:16:56 +0100

    refactor(formatters/fetch): Improve code readability
    Add withoutIndentation, urlSnippet function extractions

--- lib/formatters/fetch.ts
+++ lib/formatters/fetch.ts
@@ -3,49 +3,52 @@ import { Formatter } from '../formatter'
 import { OpenAPIObject } from '../typings/openapi';
 import { Operation } from 'lib/operation';
 
+const withoutIndentation = (s:string) => s.replace(/(\s*[\n]+)\s*/g, '\n').trim()
+
 export class FetchClientFormatter extends Formatter<Operation> {
+  url:string
+  constructor({url}:any = {}) {
+    super()
+    this.url = url
+  }
   async renderBoilerplate(apiSchema: OpenAPIObject) {
-    return [
-      `const fetch = require('node-fetch')`,
-      `const pick = (obj:any, keys:string[]) => keys.reduce((picked, key) => obj[key] !== undefined ? Object.assign(picked, {[key]: obj[key]}) : picked, {})`,
-      `const encodeQuery = (obj:any, keys:string[]) => require('querystring').encode(pick(obj, keys))`,
-      `const API_URL = ${JSON.stringify(get(apiSchema, 'servers[0].url'))}`,
-    ].join('\n')
+    const url = this.url || get(apiSchema, 'servers[0].url')
+    return withoutIndentation(`
+      const fetch = require('node-fetch')
+      const pick = (obj:any, keys:string[]) => keys.reduce((picked, key) => obj[key] !== undefined ? Object.assign(picked, {[key]: obj[key]}) : picked, {})
+      const encodeQuery = (obj:any, keys:string[]) => require('querystring').encode(pick(obj, keys))
+      const API_URL = ${JSON.stringify(url)}
+    `)
   }
 
   async render(operation:Operation) {
-    const { operationTypeName } = this.names(operation)
-    return {
-      [operationTypeName]: this.renderActionType(operation),
-    }
-  }
-  
-  renderActionType(operation:Operation) {
-    const { operationName, requestTypeName, responseTypeName } = this.names(operation)
-    return `export type ${operationName} = (payload: ${requestTypeName}) => Promise<${responseTypeName}>;`
+    const { operationTypeName, operationName, requestTypeName, responseTypeName } = this.names(operation)
+    const typedef = `export type ${operationName} = (payload: ${requestTypeName}) => Promise<${responseTypeName}>;`
+    return { [operationTypeName]: typedef }
   }
 
   async renderAction(operation:Operation) {
     const { operationName, requestTypeName, responseTypeName } = this.names(operation)
-    const queryParameterNames =
-      operation.route.parameters
-        .filter(param => param.in === 'query')
-        .map(param => param.name)
-
-    const urlCode = queryParameterNames.length ? 
-      `[API_URL, encodeQuery(body, ${JSON.stringify(queryParameterNames)})].filter(x=>x).join('?')` :
-      'API_URL'
     const fetchWrapper = `const ${operationName} =
       async (body:${requestTypeName}, options:any):Promise<${responseTypeName}> => {
-        return fetch(${urlCode}, {
+        return fetch(${this.urlSnippet(operation)}, {
           ...options,
           method: ${JSON.stringify(operation.method)},
           body: JSON.stringify(body)
         }).then((res:any) => res.json())
       }`
-    return {
-      [operationName]: fetchWrapper
-    }
+    return { [operationName]: fetchWrapper }
+  }
+
+  private urlSnippet(operation:Operation) {
+    const queryParameterNames =
+    operation.route.parameters
+      .filter(param => param.in === 'query')
+      .map(param => param.name)
+
+  return queryParameterNames.length ? 
+    `[API_URL, encodeQuery(body, ${JSON.stringify(queryParameterNames)})].filter(x=>x).join('?')` :
+    'API_URL'
   }
 
   names(operation: Operation) {
